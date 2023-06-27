ac3b2dc5fdc35c1d1a37fe1a9cceb7dd6235aa55
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-06-26 22:44:31 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Metrics for clustering'
+LONG_DESCRIPTION = 'The library was built to facilitate clustering analysis'
+
+# Setting up
+setup(
+    name="metrics",
+    version=VERSION,
+    author="Ian dos Anjos",
+    author_email="<iannaianjos@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['math', 'random'],
+    keywords=['python', 'metrics', 'clustering'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
